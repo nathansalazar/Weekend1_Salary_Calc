aed1206ef43bc4699a6f0c47cfae2c7372c013df
Nathan Salazar <nathan.salazar20@gmail.com>
2018-08-31 16:54:16 -0500

    add basic functionality for a remove button

--- client.js
+++ client.js
@@ -1,54 +1,33 @@
 let totalMonthly = 0;
 let employeeArray = [];
 
-class Employee{
-    constructor(firstName, lastName, id, title, salary){
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.id = id;
-    this.title = title;
-    this.salary = salary;
+class Employee {
+    constructor(firstName, lastName, id, title, salary) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.id = id;
+        this.title = title;
+        this.salary = salary;
     }
 }
 
-$( document ).ready( onReady );
+$(document).ready(onReady);
 
-function onReady(){
-    $('#submitButton').on('click', appendEmployee );
+function onReady() {
+    $('#submitButton').on('click', appendEmployee);
     $('.remove').hide();
+    $('#removeButton').on('click', removeEmployee);
 }
 
-function appendEmployee(){
+function appendEmployee() {
     //create employee object
     let newEmployee = new Employee($('#firstName').val(), $('#lastName').val(),
         $('#inputID').val(), $('#inputTitle').val(), $('#inputSalary').val()
     );
     //push employee into array
     employeeArray.push(newEmployee);
-    //empty table
-    $('tbody').empty();
     //add employees to table
-    for(i=0;i<employeeArray.length;i++){
-        $('tbody').append(
-            `<tr>
-                <td>`
-                    +employeeArray[i].firstName+
-                `</td>
-                <td>`
-                    +employeeArray[i].lastName+
-                `</td>
-                <td>`
-                    +employeeArray[i].id+
-                `</td>
-                <td>`
-                    +employeeArray[i].title+
-                `</td>
-                <td>`
-                    +employeeArray[i].salary+
-                `</td>
-            </tr>`)
-    }
-    
+    addEmployeesToTable();
 
     //calculate monthly salary
     calculateMonthly(newEmployee);
@@ -58,21 +37,74 @@ function appendEmployee(){
     $('.remove').show();
 }
 
-function calculateMonthly(employee){
-    let monthlySalary = Number(employee.salary)/12
-    totalMonthly += Number(monthlySalary.toFixed(2));
-    $('#totalMonthly').empty();
-    $('#totalMonthly').append('Total Monthly: $'+totalMonthly);
-    //check that total monthly is less than $20,000
-    if(totalMonthly>20000){
-        $('#totalMonthly').css('background-color','red');
+function addEmployeesToTable() {
+    //empty table
+    $('tbody').empty();
+    for (i = 0; i < employeeArray.length; i++) {
+        $('tbody').append(
+            `<tr>
+                <td>`
+            + employeeArray[i].firstName +
+            `</td>
+                <td>`
+            + employeeArray[i].lastName +
+            `</td>
+                <td>`
+            + employeeArray[i].id +
+            `</td>
+                <td>`
+            + employeeArray[i].title +
+            `</td>
+                <td>`
+            + employeeArray[i].salary +
+            `</td>
+            </tr>`)
     }
 }
 
-function clearInputFields(){
+function calculateMonthly(employee) {
+    let monthlySalary = Number(employee.salary) / 12
+    totalMonthly += Number(monthlySalary.toFixed(2));
+    $('#totalMonthly').empty();
+    $('#totalMonthly').append('Total Monthly: $' + totalMonthly);
+    //check that total monthly is less than $20,000
+    if (totalMonthly > 20000) {
+        $('#totalMonthly').css('background-color', 'red');
+    }
+}
+
+function clearInputFields() {
     $('#firstName').val('');
     $('#lastName').val('');
     $('#inputID').val('');
     $('#inputTitle').val('');
     $('#inputSalary').val('');
+}
+
+function removeEmployee() {
+    //create employee object
+    let newEmployee = new Employee($('#firstNameRemove').val(), $('#lastNameRemove').val(),
+        $('#inputIDRemove').val(), $('#inputTitleRemove').val(), $('#inputSalaryRemove').val()
+    );
+    //search for employee in array
+    let index = -1;
+    for (i = 0; i < employeeArray.length; i++) {
+        if (employeeArray[i].id === newEmployee.id) {
+            index = i;
+        }
+    }
+    if (index === -1) {
+        console.log('That person is not in the table')
+    }
+    else {
+        //remove employee from array
+        employeeArray.splice(index, 1);
+        //add employees to table
+        addEmployeesToTable();
+
+        //calculate monthly salary
+        //calculateMonthly(newEmployee);
+        //clear input fields
+        //clearInputFields();
+    }
 }
